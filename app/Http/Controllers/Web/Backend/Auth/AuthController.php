0747a0dbc8911ccc74a7fffaa8d4920fe02b5b3d
Moskov-1 <wwwronyraihan123@gmail.com>
2025-10-09 04:22:31 +0600

    fixing mismatched route names

--- app/Http/Controllers/Web/Backend/Auth/AuthController.php
+++ app/Http/Controllers/Web/Backend/Auth/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
         $user = User::create($request->validated());
 
         if( Auth::attempt(['email' => $request->email,'password'=> $request->password]) ){
-            return redirect()->route('backend.index')->with("success","registration completed successfully");
+            return redirect()->route('backend.dashboard.index')->with("success","registration completed successfully");
         }
         return back()->with(
             'error', 'Invalid credentials provided.'
