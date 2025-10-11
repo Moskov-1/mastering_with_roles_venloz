7c699dc4dfcf1a576517cf8a627be6bdce860c9a
Moskov-1 <wwwronyraihan123@gmail.com>
2025-10-11 23:52:49 +0600

    feat: jwt token auth added

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject; 
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
 
     use HasFactory, Notifiable,  HasRoles {
@@ -37,6 +38,7 @@ class User extends Authenticatable
         return $this->traitHasRole($roles, $guard);
     }
 
+    
     public function hasAnyRole($roles, $guard = null): bool
     {
         if ($this->traitHasRole('super_admin')) {
@@ -45,6 +47,19 @@ class User extends Authenticatable
         if(is_null($roles)) 
             return false;
         return $this->traitHasAnyRole($roles, $guard);
+    }
+
+    /** Get the identifier that will be stored in the subject claim of the JWT.
+     * @return mixed */
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    /** Return a key value array, containing any custom claims to be added to the JWT.
+     * @return array */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 
     protected $fillable = [
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\JWTMiddleware;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use App\Http\Middleware\adminMiddleware;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
@@ -28,5 +31,11 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => $e->getMessage(),
+                ], 401);
+            }
+        });
     })->create();
